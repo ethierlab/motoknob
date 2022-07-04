b65205293b66b9fe750ee107fa991a5f29fb498f
Christian Ethier <christian.ethier@gmail.com>
2022-07-04 11:03:26 -0400

    Revert "Update mototrak_knob_controller.m"
    This reverts commit b3137905a8df3a42104151a37099030948624e58.

--- mototrak_knob_controller.m
+++ mototrak_knob_controller.m
@@ -35,9 +35,8 @@ varNames = {'start_time', 'init_thresh', 'hit_thresh','angle',...
         'hold_time', 'duration', 'success', 'peak'};
 trial_table = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
 trial_table = addprop(trial_table,{'ver','num_rewards','num_trials','start_time','mean_peak'},{'table','table','table','table','table'});
-trial_table.Properties.CustomProperties.ver = 1.0;
+trial_table.Properties.CustomProperties.ver = 0.9;
 trial_table.Properties.CustomProperties.start_time = start_time;
-
 
 % % wait until user is ready to start
 % uiwait(msgbox('Click OK when ready to start!','Ready?','modal'));
@@ -224,7 +223,6 @@ try
     trial_table.Properties.CustomProperties.num_trials  = num_trials;
     trial_table.Properties.CustomProperties.num_rewards = num_rewards;
     trial_table.Properties.CustomProperties.mean_peak   = mpeak;
-    trial_table.Properties.CustomProperties.rat_id = rat_id;
     display_results(time_now,num_trials,num_rewards,app.num_pellets,app.man_pellets,mpeak);
     save_results(app,trial_table,crashed);
     
@@ -236,7 +234,6 @@ catch ME
     trial_table.Properties.CustomProperties.num_trials  = num_trials;
     trial_table.Properties.CustomProperties.num_rewards = num_rewards;
     trial_table.Properties.CustomProperties.mean_peak   = mpeak;
-    trial_table.Properties.CustomProperties.rat_id = rat_id;
     display_results(time_now,num_trials,num_rewards,app.num_pellets,app.man_pellets,mpeak);
     save_results(app,trial_table,crashed);
     rethrow(ME);
@@ -280,8 +277,6 @@ if strcmp(SaveButton,'Yes')
          fname = [app.rat_id.Value,'_MotoTrak_Knob_Results_',datestr(datetime('now'),'yyyymmdd_HHMMSS'),'.mat'];
          save(fullfile(app.save_dir.Value,app.rat_id.Value,fname),'trial_table');
         disp('behavior stats and parameters saved successfully');
-
-
     else
         disp('behavior stats and parameters not saved');
     end
